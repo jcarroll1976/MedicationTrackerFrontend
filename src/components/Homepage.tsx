ada06d3e7bab18f4a9cb8f345e459bb96b1c2d0e
Joshua Carroll <carrolljosh1976@gmail.com>
2024-05-13 22:06:55 -0400

    Added customized greeting on Homepage based on time of day

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+
 export default function Home() {
+    const {user} = useContext(AuthContext);
+
+    let date = new Date();
+    let greetingMessage = "";
+
+    if (date.getHours() < 12) {
+         greetingMessage = `Good Morning, ${user?.displayName}`;
+    }
+    else if (date.getHours() >= 12 && date.getHours() < 18) {
+        greetingMessage = `Good Afternoon, ${user?.displayName}`;
+    }
+    else if (date.getHours() >= 18 && date.getHours() < 24) {
+        greetingMessage = `Good Evening, ${user?.displayName}`;
+    }
+
     return (
-        <h1>This is the homepage</h1>
+        <h1>{greetingMessage}</h1>
+
     )
 }
